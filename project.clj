c5c10c26abf7e99ce84c3ea8640c03a9d8bf9210
Phil Hagelberg <technomancy@gmail.com>
2010-05-20 23:50:02 -1000

    Write entries correctly and use relative filenames.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject lein-release "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   ;; TODO: remove these deps; they're implied by lein; used for testing.
-  :dependencies [[ant/ant "1.6.5"]]
+  :dependencies [[ant/ant "1.6.5"]
+                 [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
                      [org.apache.maven/maven-ant-tasks "2.0.10"]
                      [clojure "1.2.0-master-SNAPSHOT"]
--- src/leiningen/release.clj
+++ src/leiningen/release.clj
@@ -1,17 +1,23 @@
 (ns leiningen.release
   (:use [leiningen.jar :only [jar]]
-        [clojure.contrib.io :only [file]])
+        [clojure.contrib.io :only [file to-byte-array]])
   (:import [org.apache.tools.tar TarOutputStream TarEntry]
            [java.io FileOutputStream]))
 
+(def root-count (inc (count (System/getProperty "user.dir"))))
+
 (defn- add-file [tar f]
-  (println "adding" f)
-  (.putNextEntry tar (TarEntry. f)))
+  (let [relative-file (java.io.File. (subs (.getAbsolutePath f) root-count))]
+    (when-not (.isDirectory f)
+      (.putNextEntry tar (TarEntry. relative-file))
+      (.write tar (to-byte-array f))
+      (.closeEntry tar))))
 
 (defn release [project]
   (let [jar-file (jar project)
         tar-file (file (format "%s/%s-%s.tar" (:root project)
                                (:name project) (:version project)))]
+    (.delete tar-file)
     (with-open [tar (TarOutputStream. (FileOutputStream. tar-file))]
       (doseq [p (file-seq (file (:root project)
                                 (or (:pkg-path project) "pkg")))]
@@ -19,4 +25,5 @@
       (doseq [j (filter #(re-find #"\.jar$" (.getName %))
                         (.listFiles (file (:library-path project))))]
         (add-file tar j))
-      (add-file tar (file jar-file)))))
+      (add-file tar (file jar-file)))
+    (println "Wrote" (.getName tar-file))))
