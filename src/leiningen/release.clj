98eb3bf200a964c499ca716829a471966ff4edff
Dan Larkin <dan@danlarkin.org>
2010-05-22 09:41:53 -1000

    If you create a tarbomb, the terrorists have already won.

--- src/leiningen/release.clj
+++ src/leiningen/release.clj
@@ -4,26 +4,29 @@
   (:import [org.apache.tools.tar TarOutputStream TarEntry]
            [java.io FileOutputStream]))
 
-(def root-count (inc (count (System/getProperty "user.dir"))))
+(defn entry-name [release-name f]
+  (str release-name "/" (subs (.getAbsolutePath f)
+                              (inc (count (System/getProperty "user.dir"))))))
 
-(defn- add-file [tar f]
-  (let [relative-file (java.io.File. (subs (.getAbsolutePath f) root-count))]
-    (when-not (.isDirectory f)
-      (.putNextEntry tar (TarEntry. relative-file))
+(defn- add-file [release-name tar f]
+  (when-not (.isDirectory f)
+    (let [entry (doto (TarEntry. f)
+                  (.setName (entry-name release-name f)))]
+      (.putNextEntry tar entry)
       (.write tar (to-byte-array f))
       (.closeEntry tar))))
 
 (defn release [project]
-  (let [jar-file (jar project)
-        tar-file (file (format "%s/%s-%s.tar" (:root project)
-                               (:name project) (:version project)))]
+  (let [release-name (str (:name project) "-" (:version project))
+        jar-file (jar project)
+        tar-file (file (format "%s/%s.tar" (:root project) release-name))]
     (.delete tar-file)
     (with-open [tar (TarOutputStream. (FileOutputStream. tar-file))]
       (doseq [p (file-seq (file (:root project)
                                 (or (:pkg-path project) "pkg")))]
-        (add-file tar p))
+        (add-file release-name tar p))
       (doseq [j (filter #(re-find #"\.jar$" (.getName %))
                         (.listFiles (file (:library-path project))))]
-        (add-file tar j))
-      (add-file tar (file jar-file)))
+        (add-file release-name tar j))
+      (add-file release-name tar (file jar-file)))
     (println "Wrote" (.getName tar-file))))
