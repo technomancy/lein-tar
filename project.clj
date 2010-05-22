dc76c9301b00e7ae7c2e791a197ac2aec8eb8f11
Phil Hagelberg <technomancy@gmail.com>
2010-05-22 11:55:01 -1000

    Release 1.0.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject lein-release "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  ;; TODO: remove these deps; they're implied by lein; used for testing.
-  :dependencies [[ant/ant "1.6.5"]
-                 [enlive "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+(defproject lein-release "1.0.0"
+  :description "Create a release tarball of your Leiningen projects."
+  :dev-dependencies [[swank-clojure "1.2.1"]
                      [org.apache.maven/maven-ant-tasks "2.0.10"]
+                     [ant/ant "1.6.5"]
                      [clojure "1.2.0-master-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
