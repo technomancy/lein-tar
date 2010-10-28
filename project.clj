a76c378c6c0be5ddf09cab59051dd99c9edd147b
Phil Hagelberg <technomancy@gmail.com>
2010-10-28 13:57:34 -0700

    Version 1.1.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release "1.1.2-SNAPSHOT"
+(defproject lein-release "1.1.2"
   :description "Create a release tarball of your Leiningen projects."
   :dependencies [[ant/ant "1.6.5"]]
   :eval-in-leiningen true
