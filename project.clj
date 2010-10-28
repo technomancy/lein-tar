092ffa6b86bc67bd585a5b0304868b1a622df22d
Phil Hagelberg <technomancy@gmail.com>
2010-10-28 13:56:24 -0700

    Remove nearly all dependencies; run in leiningen project.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject lein-release "1.1.1"
+(defproject lein-release "1.1.2-SNAPSHOT"
   :description "Create a release tarball of your Leiningen projects."
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.apache.maven/maven-ant-tasks "2.0.10"]
-                     [ant/ant "1.6.5"]
-                     [clojure "1.2.0-master-SNAPSHOT"]
-                     [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+  :dependencies [[ant/ant "1.6.5"]]
+  :eval-in-leiningen true
+  :min-lein-version "1.4.0")
--- src/leiningen/release.clj
+++ src/leiningen/release.clj
@@ -1,29 +1,30 @@
 (ns leiningen.release
   (:use [leiningen.jar :only [jar]]
-        [clojure.contrib.io :only [file to-byte-array]]
-        [clojure.contrib.string :only [replace-re]])
+        [clojure.java.io :only [copy]])
   (:import [org.apache.tools.tar TarOutputStream TarEntry]
-           [java.io FileOutputStream]))
+           [java.io File FileOutputStream ByteArrayOutputStream]))
 
 (defn entry-name [release-name f]
-  (let [prefix (re-pattern (str (System/getProperty "user.dir") "/(pkg/)?"))
-        stripped (replace-re prefix "" (.getAbsolutePath f))]
+  (let [prefix (str (System/getProperty "user.dir") "/(pkg/)?")
+        stripped (.replaceAll (.getAbsolutePath f) prefix "")]
     (str release-name "/" stripped)))
 
 (defn- add-file [release-name tar f]
   (when-not (.isDirectory f)
     (let [entry (doto (TarEntry. f)
-                  (.setName (entry-name release-name f)))]
+                  (.setName (entry-name release-name f)))
+          baos (ByteArrayOutputStream.)]
       (when (.canExecute f)
         ;; No way to expose unix perms? you've got to be kidding me, java!
         (.setMode entry 0755))
+      (copy f baos)
       (.putNextEntry tar entry)
-      (.write tar (to-byte-array f))
+      (.write tar (.toByteArray baos))
       (.closeEntry tar))))
 
 (defn- add-build-info [project]
   (when (System/getenv "BUILD_ID")
-    (let [build-file (file (:root project) "pkg" "build.clj")]
+    (let [build-file (File. (:root project) "pkg/build.clj")]
       (.deleteOnExit build-file)
       (spit build-file
             (str {:build-id (System/getenv "BUILD_ID")
@@ -33,14 +34,13 @@
   (add-build-info project)
   (let [release-name (str (:name project) "-" (:version project))
         jar-file (jar project)
-        tar-file (file (format "%s/%s.tar" (:root project) release-name))]
+        tar-file (File. (format "%s/%s.tar" (:root project) release-name))]
     (.delete tar-file)
     (with-open [tar (TarOutputStream. (FileOutputStream. tar-file))]
-      (doseq [p (file-seq (file (:root project)
-                                "pkg"))]
+      (doseq [p (file-seq (File. (:root project) "pkg"))]
         (add-file release-name tar p))
       (doseq [j (filter #(re-find #"\.jar$" (.getName %))
-                        (.listFiles (file (:library-path project))))]
+                        (.listFiles (File. (:library-path project))))]
         (add-file release-name tar j))
-      (add-file (str release-name "/lib") tar (file jar-file)))
+      (add-file (str release-name "/lib") tar (File. jar-file)))
     (println "Wrote" (.getName tar-file))))
