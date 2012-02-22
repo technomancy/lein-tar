fc032d8e0901309ec5b80753793f8d1b1a49d353
Phil Hagelberg <technomancy@gmail.com>
2012-02-21 21:29:30 -0800

    Add explicit dependency on ant for Leiningen 2.x.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-tar "1.0.6"
   :description "Create a tarball of your Leiningen projects."
   :eval-in-leiningen true
+  :dependencies [[org.apache.ant/ant "1.8.2"]]
   :min-lein-version "1.4.0")
