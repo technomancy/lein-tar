58f7d46f3ede805493ff33db7394d083d8e0860d
Phil Hagelberg <technomancy@gmail.com>
2013-03-27 17:35:11 -0700

    Add :url and :license.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject lein-tar "1.1.2"
+  :url "https://github.com/technomancy/lein-tar"
+  :license "Eclipse Public License 1.0"
   :description "Create a tarball of your Leiningen projects."
   :eval-in-leiningen true
   :dependencies [[org.apache.ant/ant "1.8.2"]]
