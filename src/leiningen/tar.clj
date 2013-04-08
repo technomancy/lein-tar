fb1d7c06af5cbbd448d9913ca0d69a58b7c286fc
Joe Gallo <joegallo@gmail.com>
2013-04-08 14:23:58 -0400

    Cleanup imports

--- src/leiningen/tar.clj
+++ src/leiningen/tar.clj
@@ -1,9 +1,9 @@
 (ns leiningen.tar
-  (:use [leiningen.jar :only [jar]]
-        [leiningen.uberjar :only [uberjar]]
-        [clojure.java.io :only [copy file]])
-  (:import [org.apache.tools.tar TarOutputStream TarEntry]
-           [java.io File FileOutputStream ByteArrayOutputStream]))
+  (:require [clojure.java.io :as io]
+            [leiningen.jar :as jar]
+            [leiningen.uberjar :as uberjar])
+  (:import (java.io ByteArrayOutputStream File FileOutputStream)
+           (org.apache.tools.tar TarEntry TarOutputStream)))
 
 (defn unix-path
   "Converts a File or String into a unix-like path"
@@ -31,16 +31,16 @@
       (.setMode entry 0755))
     (.putNextEntry tar entry)
     (when-not (.isDirectory f)
-      (copy f tar))
+      (io/copy f tar))
     (.closeEntry tar)))
 
 (defn- git-commit
   "Reads the value of HEAD and returns a commit SHA1."
   [git-dir]
   (when (.exists git-dir)
-    (let [head (.trim (slurp (str (file git-dir "HEAD"))))]
+    (let [head (.trim (slurp (str (io/file git-dir "HEAD"))))]
       {:git-commit (if-let [ref-path (second (re-find #"ref: (\S+)" head))]
-                     (.trim (slurp (str (file git-dir ref-path))))
+                     (.trim (slurp (str (io/file git-dir ref-path))))
                      head)})))
 
 (defn build-info [project]
@@ -49,13 +49,13 @@
     (let [hudson (when (System/getenv "BUILD_ID")
                    {:build-id (System/getenv "BUILD_ID")
                     :build-tag (System/getenv "BUILD_TAG")})
-          git (git-commit (file (:root project) ".git"))]
+          git (git-commit (io/file (:root project) ".git"))]
       (merge hudson git))))
 
 (defn- add-build-info [project]
-  (let [pkg (file (:root project) "pkg")
+  (let [pkg (io/file (:root project) "pkg")
         _ (.mkdir pkg)
-        build-file (file pkg "build.clj")
+        build-file (io/file pkg "build.clj")
         build-info (build-info project)]
     (when build-info
       (.deleteOnExit build-file)
@@ -74,26 +74,26 @@
   (second (first (filter #(= [:extension "jar"] (key %)) files))))
 
 (defn add-jars [project tar]
-  (let [j (jar project)
+  (let [j (jar/jar project)
         jar-file (if (map? j) (jar-extension j) j)]
-    (add-file (str (release-name project) "/lib") tar (file jar-file))
+    (add-file (str (release-name project) "/lib") tar (io/file jar-file))
     (doseq [j (jars-for project)]
       (add-file (release-name project) tar j))))
 
 (defn add-uberjar [project tar]
-  (let [uberjar-file (uberjar project)]
-    (doseq [:let [j (file uberjar-file)]
+  (let [uberjar-file (uberjar/uberjar project)]
+    (doseq [:let [j (io/file uberjar-file)]
             f [(.getParentFile j) j]]
       (add-file (str (release-name project) "/lib") tar f))))
 
 (defn tar [project]
   (add-build-info project)
   (let [release-name (release-name project)
-        tar-file (file (:root project) (format "%s.tar" release-name))]
+        tar-file (io/file (:root project) (format "%s.tar" release-name))]
     (.delete tar-file)
     (with-open [tar (TarOutputStream. (FileOutputStream. tar-file))]
       (.setLongFileMode tar TarOutputStream/LONGFILE_GNU)
-      (doseq [p (file-seq (file (:root project) "pkg"))]
+      (doseq [p (file-seq (io/file (:root project) "pkg"))]
         (add-file release-name tar p))
       (if (get-in project [:tar :uberjar])
         (add-uberjar project tar)
