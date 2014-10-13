fc3c4dcf6cb986d6fda4134a0aceadce6a61680c
Joe Gallo <joegallo@gmail.com>
2014-10-13 10:30:49 -0400

    Bump ant version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license "Eclipse Public License 1.0"
   :description "Create a tarball of your Leiningen projects."
   :eval-in-leiningen true
-  :dependencies [[org.apache.ant/ant "1.8.2"]]
+  :dependencies [[org.apache.ant/ant "1.9.4"]]
   :min-lein-version "2.0.0")
