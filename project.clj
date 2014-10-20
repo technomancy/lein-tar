08f43d8b471aede67bec485d045e05eb019f34fe
Joe Gallo <joegallo@gmail.com>
2014-10-20 19:40:24 -0400

    Back to development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-tar "3.3.0"
+(defproject lein-tar "3.4.0-SNAPSHOT"
   :url "https://github.com/technomancy/lein-tar"
   :license "Eclipse Public License 1.0"
   :description "Create a tarball of your Leiningen projects."
